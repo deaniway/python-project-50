2ccc2cb49c24df1c9cd233e7cf9024640dc5ff03
Denis <ad.way@mail.ri>
2023-12-24 22:16:27 +0400

    match case stylish

--- gendiff/formats/stylish.py
+++ gendiff/formats/stylish.py
@@ -5,45 +5,60 @@ NONE = '  '
 
 
 def to_str(value, spaces_count=2):
-    if value is None:
-        return "null"
-    if isinstance(value, bool):
-        return str(value).lower()
-    if isinstance(value, dict):
-        indent = SEPARATOR * (spaces_count + 4)
-        lines = []
-        for key, inner_value in value.items():
-            formatted_value = to_str(inner_value, spaces_count + 4)
-            lines.append(f"{indent}{NONE}{key}: {formatted_value}")
-        formatted_string = '\n'.join(lines)
-        end_indent = SEPARATOR * (spaces_count + 2)
-        return f"{{\n{formatted_string}\n{end_indent}}}"
-    return f"{value}"
+    match value:
+        case None:
+            return "null"
+
+        case bool_val if isinstance(bool_val, bool):
+            return str(value).lower()
+
+        case dict_val if isinstance(dict_val, dict):
+            indent = SEPARATOR * (spaces_count + 4)
+            lines = [f"{indent}{NONE}{key}: {to_str(inner_value, spaces_count + 4)}" for key, inner_value in value.items()]
+            formatted_string = '\n'.join(lines)
+            end_indent = SEPARATOR * (spaces_count + 2)
+            return f"{{\n{formatted_string}\n{end_indent}}}"
+
+        case _:
+            return f"{value}"
 
 
 def make_stylish_result(diff, spaces_count=2):
+    if not diff:
+        return '{}'
+
     indent = SEPARATOR * spaces_count
     lines = []
+
     for item in diff:
         key_name = item['name']
-        old_value = to_str(item.get("old_value"), spaces_count)
-        new_value = to_str(item.get("new_value"), spaces_count)
-        action = item["action"]
-        if action == "unchanged":
-            current_value = to_str(item.get("value"), spaces_count)
-            lines.append(f"{indent}{NONE}{key_name}: {current_value}")
-        elif action == "modified":
-            lines.append(f"{indent}{DELETE}{key_name}: {old_value}")
-            lines.append(f"{indent}{ADD}{key_name}: {new_value}")
-        elif action == "deleted":
-            lines.append(f"{indent}{DELETE}{key_name}: {old_value}")
-        elif action == "added":
-            lines.append(f"{indent}{ADD}{key_name}: {new_value}")
-        elif action == 'nested':
-            children = make_stylish_result(
-                item.get("children"), spaces_count + 4
-            )
-            lines.append(f"{indent}{NONE}{key_name}: {children}")
+        action = item['action']
+
+        match action:
+            case "unchanged":
+                current_value = to_str(item['new_value'], spaces_count)
+                lines.append(f"{indent}{NONE}{key_name}: {current_value}")
+
+            case "modified":
+                old_value = to_str(item['old_value'], spaces_count)
+                new_value = to_str(item['new_value'], spaces_count)
+                lines.extend([
+                    f"{indent}{DELETE}{key_name}: {old_value}",
+                    f"{indent}{ADD}{key_name}: {new_value}"
+                ])
+
+            case "deleted":
+                old_value = to_str(item['old_value'], spaces_count)
+                lines.append(f"{indent}{DELETE}{key_name}: {old_value}")
+
+            case "added":
+                new_value = to_str(item['new_value'], spaces_count)
+                lines.append(f"{indent}{ADD}{key_name}: {new_value}")
+
+            case "nested":
+                children = make_stylish_result(item['children'], spaces_count + 4)
+                lines.append(f"{indent}{NONE}{key_name}: {children}")
+
     formatted_string = '\n'.join(lines)
     end_indent = SEPARATOR * (spaces_count - 2)
 
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -14,3 +14,99 @@ def get_input_data(file_name):
 
 def get_expected_result(file_name):
     return read_file(file_name)
+
+
+'''
+from gendiff.generate_diff_tree import generate_diff
+import pytest
+import os
+
+
+TESTS_DIR = os.path.dirname(os.path.abspath(__file__))
+FIXTURES_PATH = f"{TESTS_DIR}/fixtures"
+
+
+def build_fixture_path(file_name):
+    return os.path.join(FIXTURES_PATH, file_name)
+
+
+def get_content(addres):
+    with open(addres, 'r') as f:
+        data = f.read()
+        return data
+
+
+@pytest.mark.parametrize("file_path1,file_path2,style,file_name", [
+    ('file_json1.json',
+     'file_json2.json',
+     'stylish',
+     'expected.txt'),
+    ('file_yaml1.yml',
+     'file_yaml2.yml',
+     'stylish',
+     'expected.txt'),
+    ('file_yaml1.yml',
+     'file_json2.json',
+     'stylish',
+     'expected.txt'),
+    ('file_json3.json',
+     'file_json4.json',
+     'stylish',
+     'expected_empty_files.txt'),
+    ('file_json3.json',
+     'file_json1.json',
+     'stylish',
+     'expected_empty_one_file.txt'),
+    ('file_json1.json',
+     'file_json3.json',
+     'stylish',
+     'expected_empty_tow_file.txt'),
+    ('file_json5.json',
+     'file_json6.json',
+     'stylish',
+     'expected_tree.txt'),
+    ('file_json5.json',
+     'file_json6.json',
+     'plain',
+     'expected_tree_plain.txt'),
+    ('file_json1.json',
+     'file_json2.json',
+     'plain',
+     'expected_plain.txt'),
+    ('file_json3.json',
+     'file_json1.json',
+     'plain',
+     'expected_empty_one_file_plain.txt'),
+    ('file_json1.json',
+     'file_json3.json',
+     'plain',
+     'expected_empty_tow_file_plain.txt'),
+    ('file_json5.json',
+     'file_json6.json',
+     'json',
+     'expected_tree_json.txt')])
+def test_generate_diff(file_path1, file_path2, style, file_name):
+    answer = get_content(build_fixture_path(file_name))
+    path1 = build_fixture_path(file_path1)
+    path2 = build_fixture_path(file_path2)
+    assert generate_diff(path1, path2, style) == answer
+
+
+@pytest.mark.parametrize("file_path1,file_path2,style,exception, exc_message", [
+    ('file_json1.json',
+     'file_json2.json',
+     'stylis',
+     ValueError,
+     "Unknown extension"),
+    ('file_json.txt',
+     'file_yaml.txt',
+     'stylish',
+     ValueError,
+     "Invalid format entered")])
+def test_generate_diff_exeption(file_path1, file_path2, style, exception, exc_message):
+    path1 = build_fixture_path(file_path1)
+    path2 = build_fixture_path(file_path2)
+    with pytest.raises(exception) as exception:
+        generate_diff(path1, path2, style)
+        assert exc_message == exception.value
+'''
