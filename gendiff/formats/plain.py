a053fa64befa5fa221ddafb898d452c62e70e1d3
Denis <ad.way@mail.ri>
2023-12-29 22:58:38 +0400

    rev 1.2

--- gendiff/formats/plain.py
+++ gendiff/formats/plain.py
@@ -1,4 +1,3 @@
-
 def to_str(value):  # неправльное добавление изменение, вкидывает везде null
     if isinstance(value, (list, dict)):
         return '[complex value]'
@@ -30,17 +29,20 @@ def make_plain_result_item(item, path=''):
                 f"From {old_value} to {new_value}"
             )
         case 'nested':
-            children = item['children']
-            return make_plain_result(children, current_path)
-        case default:
-            raise ValueError(f"Не поддерживаемый формат ноды.")
-# давай временно оставим все исключения на русском
-# исправлений много меня ждет, проще глазами отследить косяк
-# ближе к середине финиша исправим lang)
+            children = item.get('children')
+            if children is not None:
+                return make_plain_result(children, current_path)
+            else:
+                raise ValueError("Не поддерживается формат ноды")
 
 
 def make_plain_result(diff, path=''):
-    result = filter(None, map(lambda item: make_plain_result_item(item, path), diff))
+    result = []
+    for item in diff:
+        formatted_item = make_plain_result_item(item, path)
+        if formatted_item is not None:
+            result.append(formatted_item)
+
     return '\n'.join(result)
 
 
