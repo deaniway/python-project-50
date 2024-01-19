496da59ee5fef9fa762178625b9692ae0d30ecc7
Денис <121969573+deaniway@users.noreply.github.com>
2024-01-19 13:49:50 +0400

    Update generate_diff.py

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -9,18 +9,15 @@ def get_file_format(file_path):
     return ignore_name[1:]
 
 
-def parser_data_file(file_path):
-    _, file_name = os.path.splitext(file_path)
-    format_file = file_name[1:]
-
+def read_and_parse_file(file_path):
+    format_file = get_file_format(file_path)
     with open(file_path) as f:
         content = f.read()
-
     return parser_data(content, format_file)
 
 
 def generate_diff(file_path1, file_path2, formatter='stylish'):
-    data1 = parser_data_file(file_path1)
-    data2 = parser_data_file(file_path2)
+    data1 = read_and_parse_file(file_path1)
+    data2 = read_and_parse_file(file_path2)
     diff = generate(data1, data2)
     return format_diff(diff, formatter)
